91f4b084674d8a702f30a36ca3603ec1635a4fd9
Konrad Czarnota <k.czarnota27@gmail.com>
2018-05-21 22:08:30 +0200

    Change our net implementation

--- src/our_net.py
+++ src/our_net.py
@@ -1,13 +1,9 @@
-from random import seed
+import argparse
+import json
 
-import numpy as np
 from keras.layers import Dense, Conv2D, MaxPool2D, Flatten
 from keras.models import Sequential
-
-# root_directory = '../data/SET_A'
-from src.data_management import BirdsDataManager, BirdsDataGenerator
-
-root_directory = '../data/BBPP'
+from tensorflow.python.keras._impl.keras.preprocessing.image import ImageDataGenerator
 
 
 def get_model(num_classes):
@@ -15,44 +11,61 @@ def get_model(num_classes):
 
     model.add(Conv2D(filters=50, kernel_size=(3, 3), activation='relu', input_shape=(224, 224, 3)))
     model.add(Conv2D(filters=50, kernel_size=(3, 3), activation='relu'))
-    model.add(Conv2D(filters=50, kernel_size=(3, 3), activation='relu'))
-
     model.add(MaxPool2D(pool_size=(2, 2)))
 
     model.add(Conv2D(filters=50, kernel_size=(3, 3), activation='relu'))
     model.add(Conv2D(filters=50, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(filters=50, kernel_size=(3, 3), activation='relu'))
     model.add(Conv2D(filters=50, kernel_size=(3, 3), activation='relu'))
     model.add(MaxPool2D(pool_size=(2, 2)))
 
     model.add(Conv2D(filters=50, kernel_size=(3, 3), activation='relu'))
+    model.add(Conv2D(filters=50, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+
     model.add(Conv2D(filters=50, kernel_size=(3, 3), activation='relu'))
     model.add(Conv2D(filters=50, kernel_size=(3, 3), activation='relu'))
     model.add(MaxPool2D(pool_size=(2, 2)))
 
     model.add(Flatten())
-    model.add(Dense(512, activation='relu'))
     model.add(Dense(256, activation='relu'))
     model.add(Dense(128, activation='relu'))
+    model.add(Dense(64, activation='relu'))
     model.add(Dense(num_classes, activation='softmax'))
     return model
 
 
 if __name__ == '__main__':
-    np.random.seed(4)
-    seed(5)
-
-    birds_data_manager = BirdsDataManager(root_directory)
-
-    train_data, test_data = birds_data_manager.get_train_test_dataset(train_size=0.8, random_state=143)
-
-    train_generator = BirdsDataGenerator(train_data, batch_size=30)
-    test_generator = BirdsDataGenerator(test_data, batch_size=30)
+    parser = argparse.ArgumentParser(description='Train our network')
+    parser.add_argument("-train", help="Path to train data")
+    parser.add_argument("-test", help="Path to test data")
+    parser.add_argument("-history", help="File where to store history")
+    parser.add_argument("-model", help="File where to store model")
+    args = parser.parse_args()
 
     model = get_model(50)
+    datagen = ImageDataGenerator()
+
+    train_generator = datagen.flow_from_directory(
+        args.train,
+        target_size=(224, 224),
+        batch_size=16,
+        class_mode='categorical')
+
+    test_generator = datagen.flow_from_directory(
+        args.test,
+        target_size=(224, 224),
+        batch_size=16,
+        class_mode='categorical')
 
     model.compile(optimizer='rmsprop', loss='categorical_crossentropy',
                   metrics=['categorical_accuracy', 'top_k_categorical_accuracy'])
-    model.fit_generator(generator=train_generator.generate(), steps_per_epoch=20, epochs=20,
-                    validation_data=test_generator.generate(), validation_steps=5)
+    history = model.fit_generator(generator=train_generator, steps_per_epoch=64, epochs=64,
+                    validation_data=test_generator, validation_steps=5)
 
-    model.save('birdsmodel_our_bounding.h5')
+    with open(args.history, 'w') as f:
+        json.dump(history, f)
+
+    model.save(args.model)
